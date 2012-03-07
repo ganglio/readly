c0cf4d10d76f1acbafc1972a959f08973be1acda
Roberto Torella <robertot@square-enix.com>
2012-03-07 18:36:46 +0000

    Login template + some more stuff

--- scripts/header.view.js
+++ scripts/header.view.js
@@ -2,17 +2,19 @@ $(document).ready(function(){
 	HeaderView = Backbone.View.extend({
 		el:"header",
 		initialize: function(){
-			this.render();
+			$.get("user/isIn",{},function(isIn){
+				if (isIn) {
+					$("header .buttons").load("tpl/isIn.tpl.html");
+				} else {
+					$("header .buttons").load("tpl/isNotIn.tpl.html");
+				}
+			},"json");
 		},
 		events: {
 			"click .login":"login"
 		},
-		render:function(){
-			console.log(this.el);
-			return this;
-		},
 		login:function(){
-			
+			$("#modal").load("tpl/login.tpl.html",.slideDown();
 		}
 	});
 	
