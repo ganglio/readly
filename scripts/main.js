10278b199e157aae4615969acfcb10df9a00ea65
Roberto Torella <robertot@square-enix.com>
2012-03-08 19:18:40 +0000

    local storage + ajax sync

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,29 @@
 $(document).ready(function(){
+	// Load current library
 	var library=$.jStorage.get("library");
 	
-	if (!library) {
-		$.get("library/getbooks",{},function(books){
+	// Update library
+	$.ajax({
+		"url":"library/getbooks",
+		"type":"get",
+		"timeout":5000,
+		"dataType":"json",
+		"beforeSend":function(){
+			$("#trobbler").fadeIn("fast");
+		},
+		"success":function(books){
 			$.jStorage.set("library",books);
 			library=books;
-		},"json");
-	}
+		},
+		"error":function(){
+			console.log("error");
+		},
+		"complete":function(){
+			for (var i in library)
+				$("section").append('<article class="book"><img class="cover" src="'+library[i].cover+'" /><h2>'+library[i].author+'</h2><h1>'+library[i].title+'</h1></article>');
+			$("#trobbler").fadeOut("fast");
+		}
+	});
+	
+	
 });
